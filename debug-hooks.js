672464e1e94af66ef0975e847d3b7db33922d424
syakesaba <syakesaba@gmail.com>
2022-11-05 08:46:07 +0900

    calle name

--- debug-hooks.js
+++ debug-hooks.js
@@ -28,23 +28,103 @@ window.plugin.debughooks = function() {};
 
 //--------------------------SCRIPT BEGIN
 
-window.plugin.debughooks.logging = function() {
-   console.log(arguments);
+window.plugin.debughooks.portalSelected = function() {
+    console.log(arguments.callee.name);
+}
+
+window.plugin.debughooks.mapDataRefreshStart = function() {
+    console.log(arguments.callee.name);
+}
+
+window.plugin.debughooks.mapDataEntityInject = function() {
+    console.log(arguments.callee.name);
+}
+
+window.plugin.debughooks.mapDataRefreshEnd = function() {
+    console.log(arguments.callee.name);
+}
+
+window.plugin.debughooks.linkAdded = function() {
+    console.log(arguments.callee.name);
+}
+
+window.plugin.debughooks.fieldAdded = function() {
+    console.log(arguments.callee.name);
+}
+
+window.plugin.debughooks.portalRemoved = function() {
+    console.log(arguments.callee.name);
+}
+
+window.plugin.debughooks.linkRemoved = function() {
+    console.log(arguments.callee.name);
+}
+
+window.plugin.debughooks.fieldRemoved = function() {
+    console.log(arguments.callee.name);
+}
+
+window.plugin.debughooks.portalDetailsUpdated = function() {
+    console.log(arguments.callee.name);
+}
+
+window.plugin.debughooks.publicChatDataAvailable = function() {
+    console.log(arguments.callee.name);
+}
+
+window.plugin.debughooks.factionChatDataAvailable = function() {
+    console.log(arguments.callee.name);
+}
+
+window.plugin.debughooks.alertsChatDataAvailable = function() {
+    console.log(arguments.callee.name);
+}
+
+window.plugin.debughooks.requestFinished = function() {
+    console.log(arguments.callee.name);
+}
+
+window.plugin.debughooks.iitcLoaded = function() {
+    console.log(arguments.callee.name);
+}
+
+window.plugin.debughooks.portalDetailLoaded = function() {
+    console.log(arguments.callee.name);
+}
+
+window.plugin.debughooks.paneChanged = function() {
+    console.log(arguments.callee.name);
+}
+
+window.plugin.debughooks.artifactsUpdated = function() {
+    console.log(arguments.callee.name);
+}
+
+window.plugin.debughooks.nicknameClicked = function() {
+    console.log(arguments.callee.name);
+}
+
+window.plugin.debughooks.geoSearch = function() {
+    console.log(arguments.callee.name);
+}
+
+window.plugin.debughooks.search = function() {
+    console.log(arguments.callee.name);
 }
 
 window.plugin.debughooks.main = function() {
 //    //https://github.com/IITC-CE/ingress-intel-total-conversion/blob/master/core/code/hooks.js
-//    addHook('portalSelected', window.plugin.debughooks.logging);
+    addHook('portalSelected', window.plugin.debughooks.portalSelected);
 // portalSelected: called when portal on map is selected/unselected.
 //              Provide guid of selected and unselected portal.
-//    addHook('mapDataRefreshStart', window.plugin.debughooks.logging);
+    addHook('mapDataRefreshStart', window.plugin.debughooks.mapDataRefreshStart);
 // mapDataRefreshStart: called when we start refreshing map data
-//    addHook('mapDataEntityInject', window.plugin.debughooks.logging);
+    addHook('mapDataEntityInject', window.plugin.debughooks.mapDataEntityInject);
 // mapDataEntityInject: called just as we start to render data. has callback to
 //                      Sinject cached entities into the map render
-//    addHook('mapDataRefreshEnd', window.plugin.debughooks.logging);
+    addHook('mapDataRefreshEnd', window.plugin.debughooks.mapDataRefreshEnd);
 // mapDataRefreshEnd: called when we complete the map data load
-    addHook('portalAdded', window.plugin.debughooks.logging);
+    addHook('portalAdded', window.plugin.debughooks.portalAdded);
 // portalAdded: called when a portal has been received and is about to
 //              be added to its layer group. Note that this does NOT
 //              mean it is already visible or will be, shortly after.
@@ -52,60 +132,60 @@ window.plugin.debughooks.main = function() {
 //              shown at all. Injection point is in
 //              code/map_data.js#renderPortal near the end. Will hand
 //              the Leaflet CircleMarker for the portal in "portal" var.
-//    addHook('linkAdded', window.plugin.debughooks.logging);
+    addHook('linkAdded', window.plugin.debughooks.linkAdded);
 // linkAdded:   called when a link is about to be added to the map
-//    addHook('fieldAdded', window.plugin.debughooks.logging);
+    addHook('fieldAdded', window.plugin.debughooks.fieldAdded);
 // fieldAdded:  called when a field is about to be added to the map
-//    addHook('portalRemoved', window.plugin.debughooks.logging);
+    addHook('portalRemoved', window.plugin.debughooks.portalRemoved);
 // portalRemoved: called when a portal has been removed
-//    addHook('linkRemoved', window.plugin.debughooks.logging);
+    addHook('linkRemoved', window.plugin.debughooks.linkRemoved);
 // linkRemoved: called when a link has been removed
-//    addHook('fieldRemoved', window.plugin.debughooks.logging);
+    addHook('fieldRemoved', window.plugin.debughooks.fieldRemoved);
 // fieldRemoved: called when a field has been removed
-//    addHook('portalDetailsUpdated', window.plugin.debughooks.logging);
+    addHook('portalDetailsUpdated', window.plugin.debughooks.portalDetailsUpdated);
 // portalDetailsUpdated: fired after the details in the sidebar have
 //              been (re-)rendered Provides data about the portal that
 //              has been selected.
-//    addHook('publicChatDataAvailable', window.plugin.debughooks.logging);
+    addHook('publicChatDataAvailable', window.plugin.debughooks.publicChatDataAvailable);
 // publicChatDataAvailable: this hook runs after data for any of the
 //              public chats has been received and processed, but not
 //              yet been displayed. The data hash contains both the un-
 //              processed raw ajax response as well as the processed
 //              chat data that is going to be used for display.
-//    addHook('factionChatDataAvailable', window.plugin.debughooks.logging);
+    addHook('factionChatDataAvailable', window.plugin.debughooks.factionChatDataAvailable);
 // factionChatDataAvailable: this hook runs after data for the faction
 //              chat has been received and processed, but not yet been
 //              displayed. The data hash contains both the unprocessed
 //              raw ajax response as well as the processed chat data
 //              that is going to be used for display.
-//    addHook('alertsChatDataAvailable', window.plugin.debughooks.logging);
+    addHook('alertsChatDataAvailable', window.plugin.debughooks.alertsChatDataAvailable);
 // alertsChatDataAvailable: this hook runs after data for the alerts
 //              chat has been received and processed, but not yet been
 //              displayed. The data hash contains both the unprocessed
 //              raw ajax response as well as the processed chat data
 //              that is going to be used for display.
-    //addHook('requestFinished', window.plugin.debughooks.logging);
+    addHook('requestFinished', window.plugin.debughooks.requestFinished);
 // requestFinished: DEPRECATED: best to use mapDataRefreshEnd instead
 //              called after each map data request finished. Argument is
 //              {success: boolean} indicated the request success or fail.
-//    addHook('iitcLoaded', window.plugin.debughooks.logging);
+    addHook('iitcLoaded', window.plugin.debughooks.iitcLoaded);
 // iitcLoaded: called after IITC and all plugins loaded
-//    addHook('portalDetailLoaded', window.plugin.debughooks.logging);
+    addHook('portalDetailLoaded', window.plugin.debughooks.portalDetailLoaded);
 // portalDetailLoaded: called when a request to load full portal detail
 //              completes. guid, success, details parameters
-//    addHook('paneChanged', window.plugin.debughooks.logging);
+    addHook('paneChanged', window.plugin.debughooks.paneChanged);
 // paneChanged  called when the current pane has changed. On desktop,
 //              this only selects the current chat pane; on mobile, it
 //              also switches between map, info and other panes defined
 //              by plugins
-//    addHook('artifactsUpdated', window.plugin.debughooks.logging);
+    addHook('artifactsUpdated', window.plugin.debughooks.artifactsUpdated);
 // artifactsUpdated: called when the set of artifacts (including targets)
 //              has changed. Parameters names are old, new.
-//    addHook('nicknameClicked', window.plugin.debughooks.logging);
+    addHook('nicknameClicked', window.plugin.debughooks.nicknameClicked);
 // nicknameClicked:
-//    addHook('geoSearch', window.plugin.debughooks.logging);
+    addHook('geoSearch', window.plugin.debughooks.geoSearch);
 // geoSearch:
-//    addHook('search', window.plugin.debughooks.logging);
+    addHook('search', window.plugin.debughooks.search);
 // search:
 }
 
